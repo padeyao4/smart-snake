450fd8df5ff344166648a4bf8d3ef680ce98eb5b
guojian <guojian_k@qq.com>
2021-08-11 00:36:37 +0800

    update

--- desktop/src/main/snake/desktop/DesktopLauncher.java
+++ desktop/src/main/snake/desktop/DesktopLauncher.java
@@ -6,14 +6,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import snake.SmartSnake;
 
 public class DesktopLauncher {
-	public static void main(String[] args) {
-		var configuration = new LwjglApplicationConfiguration();
-		configuration.height=500;
-		configuration.width=500;
-		configuration.title="smart-snake";
-		configuration.addIcon("snake_128.png", Files.FileType.Classpath);
-		configuration.resizable=false;
-		configuration.forceExit=true;
-		new LwjglApplication(new SmartSnake(), configuration);
-	}
+    public static void main(String[] args) {
+        var configuration = new LwjglApplicationConfiguration();
+        configuration.height = 500;
+        configuration.width = 500;
+        configuration.title = "smart-snake";
+        configuration.addIcon("snake_128.png", Files.FileType.Classpath);
+        configuration.resizable = false;
+        configuration.forceExit = false;
+        new LwjglApplication(new SmartSnake(), configuration);
+    }
 }
