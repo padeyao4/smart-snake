dd501714bf2fdb6c480e365e0bdca44dd8a712c7
guojian <guojian_k@qq.com>
2021-08-18 10:24:38 +0800

    update

--- core/src/guojian/MenuScreen.java
+++ core/src/guojian/MenuScreen.java
@@ -3,7 +3,6 @@ package guojian;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -44,6 +43,12 @@ public class MenuScreen extends ScreenAdapter {
         return playButton;
     }
 
+    /**
+     * 获取label style
+     *
+     * @param smartSnake
+     * @return
+     */
     private Label.LabelStyle getTitleStyle(SmartSnake smartSnake) {
         var bitmapFont = smartSnake.skin.getFont("font");
         bitmapFont.getData().setScale(1.5f);
