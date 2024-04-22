736cdd7cd2a4c7f2950d9e34acfca76225f9ca9f
Jian Guo <guojian_k@qq.com>
2024-04-22 13:58:25 +0800

    refactor

---
+++ core/src/guojian/core/BrainImpl.java
@@ -0,0 +1,19 @@
+package guojian.core;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BrainImpl implements Brain {
+
+    /**
+     * 搜索全图，找到所有路径
+     */
+    private Optional<List<Point>> dfs(Snake snake) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Point>> search(Snake snake) {
+        return Optional.empty();
+    }
+}
